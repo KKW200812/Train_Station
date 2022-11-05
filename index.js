f4f2e006eb5886870c0844a815ca17a730133fdd
KKW200812 <116416301+KKW200812@users.noreply.github.com>
2022-11-05 11:19:41 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,19 @@
+let saveEl = document.getElementById("save-el")
+let countEl = document.getElementById("count-el")
+let count = 0
+let smallTotal = 0
+let totalEl = document.getElementById("total-el")
+
+
+function increment() {
+    count += 1
+    countEl.textContent = count
+}
+
+function save() {
+    let countStr = count + " - "
+    saveEl.textContent += countStr
+    smallTotal += saveEl
+    countEl.textContent = 0
+    count = 0
+}
